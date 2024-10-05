a8a0e0bb1f8910adcfff7e8dfd710a3410143e69
Nathan Jones <nsjones021@gmail.com>
2024-10-05 17:57:39 +0000

    Add Script.js Listed all functions for project

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+    
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAddition() {
+    
+}
+
+function displaySubtract() {
+    
+}
+
+function displayMultiply() {
+    
+}
+
+function displayDivide() {
+    
+}
