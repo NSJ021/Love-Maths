ac04341e9707ceafeefa6b3a949f9e9d60e4cc26
Nathan Jones <nsjones021@gmail.com>
2024-10-05 18:22:15 +0000

    Add: script.js setup event listeners and run game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
+// Wait for DOM to load
+//  Get button elements and add event listeners
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    // returns an array of all elements matching the tag name, e.g all the buttons on the page
+    let buttons = document.getElementsByTagName("button");
+
+
+    // For loop that loops through array of buttons, then checks what button was pressed with getAttribute("data-type")
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You Clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You Clicked ${gameType}`);
+            }
+        })
+    }
+
+})
+
+/**
+ * Main Game Loop, called when initially loaded
+ * and after an answer is processed
+ */
 function runGame() {
-    
+    // Random numbers used to create questions
+    let num1 = Math.floor(Math.random() * 100) + 1;
+    let num2 = Math.floor(Math.random() * 100) + 1;
 }
 
 function checkAnswer() {
